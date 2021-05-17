db9923c41aa346c6bc46c35ff9158e51ab6486b2
Danny <codesmith@jdanielmartinez.com>
2021-05-17 14:39:33 -0700

    Add boilerplate code
    Co-authored-by: Jono <jonocr@users.noreply.github.com>
    Co-authored-by: Kevin <xkevinpark@users.noreply.github.com>
    Co-authored-by: Kai <kaijosefevans@users.noreply.github.com>

---
+++ src/client/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render } from 'react-dom';
+// import { BrowserRouter } from 'react-router-dom';
+// eslint-disable-next-line import/extensions
+import App from './App';
+
+render(
+  <App />,
+  document.getElementById('main'),
+);
