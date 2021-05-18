d8416958ba137c41954571174cb4ede9cda99b6a
Jonathan Calvo <jono.calvo@gmail.com>
2021-05-17 17:27:15 -0700

    Added model and controllers
    Co-authored-by: Kevin <xkevinpark@users.noreply.github.com>
    Co-authored-by: Danny <J-DannyMartinez@users.noreply.github.com>
    Co-authored-by: Kai <kaijosefevans@users.noreply.github.com>

---
+++ src/server/models/LetsGoModel.js
@@ -0,0 +1,14 @@
+const { Pool } = require('pg');
+require('dotenv').config();
+
+// create a new pool here using the connection string above
+const pool = new Pool({
+  connectionString: process.env.SQL_URI,
+});
+
+module.exports = {
+  query: (text, params, callback) => {
+    console.log('executed query', text, params);
+    return pool.query(text, params, callback);
+  }
+};
