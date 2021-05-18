f23acf5b6b3c05fa6cce3f69c1f3ca0485159cc3
Danny <codesmith@jdanielmartinez.com>
2021-05-18 14:46:56 -0700

    Start work on signup page

--- src/server/controllers/userController.js
+++ src/server/controllers/userController.js
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
 /* eslint-disable camelcase */
 /* eslint-disable max-len */
-=======
->>>>>>> main
 const bcrypt = require('bcrypt');
 const db = require('../models/LetsGoModel');
 
@@ -12,11 +9,7 @@ const userController = {};
 
 // Create a userController as middleware to pass it userRouter:
 userController.addUser = (req, res, next) => {
-<<<<<<< HEAD
   // safety feature: VALUES ($1, $2, $3, $4, $5)
-=======
-  // safety feature: VALUES ($1, $2, $3, $4, $5)
->>>>>>> main
   // sanitizes i.e. saves from hackers...
   const query = `INSERT INTO "public"."Users" (first_name, last_name, email, password)
     VALUES ($1, $2, $3, $4) RETURNING *`;
@@ -40,11 +33,7 @@ userController.addUser = (req, res, next) => {
     .catch((err) => next({
       log: 'error in addUser controller',
       status: 500,
-<<<<<<< HEAD
       message: { err },
-=======
-      message: { err }
->>>>>>> main
     }));
 };
 // bcrypt middleware:
