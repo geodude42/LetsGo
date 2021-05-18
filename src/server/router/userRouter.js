49e95696f0a6128364ef112a9027581388c1d523
Jonathan Calvo <jono.calvo@gmail.com>
2021-05-17 17:24:21 -0700

    Added router for users to server
    Co-authored-by: Kevin <xkevinpark@users.noreply.github.com>
    Co-authored-by: Danny <J-DannyMartinez@users.noreply.github.com>
    Co-authored-by: Kai <kaijosefevans@users.noreply.github.com>

---
+++ src/server/router/userRouter.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+
+// Controllers
+const userController = require('../controllers/userController');
+const cookieController = require('../controllers/cookieController');
+
+// Make a POST request to signup:
+router.post('/signup', userController.bcrypt, userController.addUser, (req, res) => {
+  // res.locals.users is the data in the query:
+  res.status(200).send(res.locals.users);
+});
+// Make a POST request to signin:
+router.post('/signin', userController.login, cookieController.setSSIDCookie, (req, res) => {
+  //TODO add cookie is logged
+  res.status(200).send(res.locals.users);
+});
+
+module.exports = router;
