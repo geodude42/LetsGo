1e0239a70d24dcc1368126e6e55bc529b2d144b2
Danny <codesmith@jdanielmartinez.com>
2021-05-19 08:53:58 -0700

    Add styling, text fields to signup

--- src/client/components/signup.jsx
+++ src/client/components/signup.jsx
@@ -1,5 +1,30 @@
 import React, { useReducer } from 'react';
-import { Paper, TextField, Typography } from '@material-ui/core';
+import {
+  Paper, TextField, Typography, Button,
+} from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+  paper: {
+    display: 'flex',
+    maxWidth: '50%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    minHeight: '60vh',
+    margin: 'auto',
+    flexWrap: 'wrap',
+  },
+  container: {
+    '& > *': {
+      margin: '5px',
+    },
+  },
+  btn: {
+    justifySelf: 'flex-end',
+    margin: '2vh 0 0 38vh',
+  },
+
+});
 
 const Signup = (props) => {
   const [formInput, setFormInput] = useReducer(
@@ -12,9 +37,13 @@ const Signup = (props) => {
     },
   );
 
+  const classes = useStyles();
+
   const handleSubmit = (e) => {
+    console.log('submit hit');
     e.preventDefault();
     const data = { formInput };
+    console.log(data);
 
     // TODO: Fetch to update DB
     // TODO: Follow up fetch with redirect to global feed or re-signup
@@ -27,19 +56,57 @@ const Signup = (props) => {
   };
 
   return (
-    <Paper>
-      <Typography variant="h5">
-        Sign Up
-      </Typography>
-      <form onSubmit={handleSubmit}>
-        <TextField
-          label="Email"
-          name="email"
-          defaultValue={formInput.email}
-          helperText="e.g. name@gmail.com"
-          onChange={handleInput}
-        />
-      </form>
+    <Paper className={classes.paper}>
+      <div>
+        <div>
+          <Typography variant="h5" className={classes.signUp}>
+            Sign Up
+          </Typography>
+        </div>
+        <form onSubmit={handleSubmit}>
+          <div className={classes.container}>
+            <TextField
+              className={classes.text}
+              label="Email"
+              name="email"
+              margin="normal"
+              defaultValue={formInput.email}
+              helperText="e.g. name@gmail.com"
+              onChange={handleInput}
+            />
+            <TextField
+              className={classes.text}
+              margin="normal"
+              label="Password"
+              name="password"
+              defaultValue={formInput.password}
+              onChange={handleInput}
+            />
+
+          </div>
+
+          <div className={classes.container}>
+            <TextField
+              className={classes.text}
+              label="First Name"
+              name="firstName"
+              defaultValue={formInput.firstName}
+              helperText="John"
+              onChange={handleInput}
+            />
+            <TextField
+              className={classes.text}
+              label="Last Name"
+              name="lastName"
+              defaultValue={formInput.lastName}
+              helperText="Doe"
+              onChange={handleInput}
+            />
+
+          </div>
+          <Button variant="contained" className={classes.btn} color="secondary"> Click Me </Button>
+        </form>
+      </div>
     </Paper>
   );
 };
