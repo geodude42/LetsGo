8ff13c629acfb622f4fa6cba8fc3c801ef1f655a
Danny <codesmith@jdanielmartinez.com>
2021-05-19 08:54:17 -0700

    Fix remnant App export error

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 // import routes from './containers/routes';
 import GlobalFeed from './GlobalFeed';
-import Signin from './components/signin';
+// import Signin from './components/signin';
 import Signup from './components/signup';
 
 function App() {
@@ -11,15 +11,9 @@ function App() {
     <BrowserRouter>
       <Route exact path="/" component={GlobalFeed} />
       <Route path="/signup" component={Signup} />
-      <Route path="/signin" component={Signin} />
+      {/* <Route path="/signin" component={Signin} /> */}
     </BrowserRouter>
   );
 }
 
-export default (
-  <BrowserRouter>
-    <Route exact path="/" component={GlobalFeed} />
-    <Route path="/signup" component={Signup} />
-    <Route path="/signin" component={Signin} />
-  </BrowserRouter>
-);
+export default App;
