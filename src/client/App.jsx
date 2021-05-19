f9b4547c5fae9c8a1ed81683181cda5032b66948
Danny <codesmith@jdanielmartinez.com>
2021-05-18 18:45:39 -0700

    Add correct Route information

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
-import routes from './containers/routes';
+// import routes from './containers/routes';
+import GlobalFeed from './GlobalFeed';
+import Signin from './components/signin';
+import Signup from './components/signup';
 
 function App() {
+  console.log('Made it to App');
   return (
     <BrowserRouter>
-      <Route routes={routes} />
+      <Route exact path="/" component={GlobalFeed} />
+      <Route path="/signup" component={Signup} />
+      <Route path="/signin" component={Signin} />
     </BrowserRouter>
   );
 }
 
-export default App;
+export default (
+  <BrowserRouter>
+    <Route exact path="/" component={GlobalFeed} />
+    <Route path="/signup" component={Signup} />
+    <Route path="/signin" component={Signin} />
+  </BrowserRouter>
+);
